e31198cec6c21d107e31de13203a0e805d9b7ef3
mpadge <mark.padgham@email.com>
2022-01-19 17:06:49 +0100

    add daily.bash script

---
+++ daily.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+NC='\033[0m'
+ARG='\033[0;31m' # red
+TXT='\033[0;32m' # green, or 1;32m for light green
+SYM='\u2192' # right arrow
+
+if [ "$1" == "" ]; then
+    Rscript -e "calcurseR::cc_update_daily()"
+elif [ "$1" == "help" ]; then
+    echo -e "${SYM} ${ARG}no arguments${NC}    : ${TXT}update daily task list from calendar.${NC}"
+    echo -e "${SYM} ${ARG}help${NC}            : ${TXT}display these help messages.${NC}"
+    echo -e "${SYM} ${ARG}notes${NC}           : ${TXT}update daily task list from todo notes.${NC}"
+elif [ "$1" == "notes" ]; then
+    Rscript -e "calcurseR::cc_update_notes()"
+else
+    echo -e "daily on accepts 'notes'; see 'daily --help' for help"
+fi
